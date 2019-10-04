ea6e15a54ae99d21981d147f64c560ecfeb957e1
Guido Schöpping <gusco@schopping.net>
2019-10-04 20:06:16 +0200

    Hello World

---
+++ src/nl/schoepping/Main.java
@@ -0,0 +1,9 @@
+package nl.schoepping;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World");
+	    System.out.println("Another comment");
+    }
+}
