d5d8eee8ec103e7793b03ea3b3a6276688aa29f8
guido <guido@schoepping.nl>
2021-12-18 15:28:34 +0100

    First App

--- app/src/main/java/net/schopping/helloworld/MainActivity.java
+++ app/src/main/java/net/schopping/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package net.schopping.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: complete file
     }
+
+    public void onClickbtnRegister(View view) {
+        EditText edFirstName = findViewById(R.id.edFirstName);
+        EditText edLastName = findViewById(R.id.edLastName);
+        EditText edEmail = findViewById(R.id.edEmail);
+        TextView txtFirstName = findViewById(R.id.txtFirstName);
+        TextView txtLastName = findViewById(R.id.txtLastName);
+        TextView txtEmail = findViewById(R.id.txtEmail);
+        txtFirstName.setText("Voornaam: " + edFirstName.getText().toString());
+        txtLastName.setText("Achternaam: " + edLastName.getText().toString());
+        txtEmail.setText("E-mail: " + edEmail.getText().toString());
+
+    }
+
 }
